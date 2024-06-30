1fb7b1f5242f4c6834278904bffcd7685bd548c6
Aaron Carlson <acarlson2@carthage.edu>
2024-06-30 11:49:08 -0500

    exercise 2 complete

---
+++ BestBuyBestPractices/BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using Dapper;
+
+namespace BestBuyBestPractices
+{
+	public class DapperProductRepository: IProductRepository
+	{
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection)
+		{
+			_connection = connection;
+		}
+
+		public IEnumerable<Product> GetAllProducts()
+		{
+			return _connection.Query<Product>("SELECT * FROM Products;");
+		}
+
+		public void CreateProduct(string name, double price, int categoryID)
+		{
+			_connection.Execute("INSERT INTO Products (Name, Price, CategoryID) " +
+				"VALUES (@name, @price, @categoryID);",
+				new { name = name, price = price, categoryID = categoryID });
+		}
+
+    }
+}
+
--- BestBuyBestPractices/BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/BestBuyBestPractices/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
+using BestBuyBestPractices;
 
 var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -11,3 +12,22 @@ string connString = config.GetConnectionString("DefaultConnection");
 
 IDbConnection conn = new MySqlConnection(connString);
 
+var repo = new DapperProductRepository(conn);
+
+Console.WriteLine("What is the name of your new product?");
+var prodName = Console.ReadLine();
+
+Console.WriteLine("What is the price?");
+var prodPrice = double.Parse(Console.ReadLine());
+
+Console.WriteLine("What is the Category ID?");
+var prodCat = int.Parse(Console.ReadLine());
+
+repo.CreateProduct(prodName, prodPrice, prodCat);
+
+var prodList = repo.GetAllProducts();
+
+foreach(var prod in prodList)
+{
+    Console.WriteLine($"{prod.ProductID} - {prod.Name}");
+}
